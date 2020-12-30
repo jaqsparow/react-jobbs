9a3758ca3f7b7829d3a8866eb6fc002ccf58569a
jaqsparow <jaqsparow@yahoo.com>
2020-12-30 11:31:05 -0700

    correct credit svg

--- src/components/Credit/Credit.jsx
+++ src/components/Credit/Credit.jsx
@@ -2,18 +2,18 @@ import React from "react";
 const Credit = () => {
   return (
     <React.Fragment>
-      <div className="flex justify-center">
-        <p className="text-gray-500 text-sm text-center">© 2020 Jobbs.info</p>
-        <p className="text-gray-500 px-5">Contact me at</p>
+      <div className="flex justify-center text-sm text-gray-500">
+        <p className="px-2">© 2020 Jobbs.info</p>
+        <p className="px-2">Contact me at</p>
 
         <a
-          className="text-gray-500"
+          className=""
           href="https://twitter.com/jaqsparow"
           target="_blank"
           rel="noreferrer"
         >
           <svg
-            className="text-gray-400 fill-current h-5 w-5"
+            className="text-gray-400 align-middle inline-block fill-current h-4 w-4"
             viewBox="0 0 512 512"
           >
             <path d="M512 97.248c-19.04 8.352-39.328 13.888-60.48 16.576 21.76-12.992 38.368-33.408 46.176-58.016-20.288 12.096-42.688 20.64-66.56 25.408C411.872 60.704 384.416 48 354.464 48c-58.112 0-104.896 47.168-104.896 104.992 0 8.32.704 16.32 2.432 23.936-87.264-4.256-164.48-46.08-216.352-109.792-9.056 15.712-14.368 33.696-14.368 53.056 0 36.352 18.72 68.576 46.624 87.232-16.864-.32-33.408-5.216-47.424-12.928v1.152c0 51.008 36.384 93.376 84.096 103.136-8.544 2.336-17.856 3.456-27.52 3.456-6.72 0-13.504-.384-19.872-1.792 13.6 41.568 52.192 72.128 98.08 73.12-35.712 27.936-81.056 44.768-130.144 44.768-8.608 0-16.864-.384-25.12-1.44C46.496 446.88 101.6 464 161.024 464c193.152 0 298.752-160 298.752-298.688 0-4.64-.16-9.12-.384-13.568 20.832-14.784 38.336-33.248 52.608-54.496z" />
